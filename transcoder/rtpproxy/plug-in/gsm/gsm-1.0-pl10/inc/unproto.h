1df67d5fbf9d624ba3aaa950b8b0e2c018dbf8ca
sayer <sayer>
2006-02-16 19:52:04 +0000

    initial import of the codecs

---
+++ transcoder/rtpproxy/plug-in/gsm/gsm-1.0-pl10/inc/unproto.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 1992 by Jutta Degener and Carsten Bormann, Technische
+ * Universitaet Berlin.  See the accompanying file "COPYRIGHT" for
+ * details.  THERE IS ABSOLUTELY NO WARRANTY FOR THIS SOFTWARE.
+ */
+
+/*$Header: /home/xubuntu/berlios_backup/github/tmp-cvs/semsivr/Repository/transcoder/rtpproxy/plug-in/gsm/gsm-1.0-pl10/inc/unproto.h,v 1.1 2006/02/16 19:52:04 sayer Exp $*/
+
+#ifdef	PROTO_H		/* sic */
+#undef	PROTO_H
+
+#undef	P
+#undef	P0
+#undef	P1
+#undef	P2
+#undef	P3
+#undef	P4
+#undef	P5
+#undef	P6
+#undef	P7
+#undef	P8
+
+#endif	/* PROTO_H */
